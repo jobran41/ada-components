1c59e0799130d4f1d474002bc5544868c4e67ed1
ines ben romdhane <inesbenromdhane@MacBook-Pro-de-ines.local>
2018-09-03 19:01:14 +0100

    add style to progress bar

--- src/components/ada-progress/progress.js
+++ src/components/ada-progress/progress.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from "prop-types"
 import { LinearProgress } from "react-md"
 
+import './style.scss'
+
 export default class Progress extends Component {
   render() {
     const { item } = this.props
@@ -11,8 +13,10 @@ export default class Progress extends Component {
           id={`item-LinearProgress-${item.id}`}
           value={item.value}
         />
-        <div className="item-percent-min">%{item.min}</div>
-        <div className="item-percent-max">%{item.max}</div>
+        <div className="item-percent-wrapper">
+          <div className="item-percent-min">%{item.min}</div>
+          <div className="item-percent-max">{item.value}% / {item.max}%</div>
+        </div>
       </div>
     )
   }
