65b38467cb4ffddd8f9dfb2e72af1820ace4eb52
ines ben romdhane <inesbenromdhane@MacBook-Pro-de-ines.local>
2018-09-22 13:36:03 +0100

    add docs to media page

--- src/pages/media/media.js
+++ src/pages/media/media.js
@@ -4,6 +4,12 @@ import { Button } from "react-md"
 import PanelProfile from 'components/panel-profile'
 import SupportAssets from 'components/support-assets'
 import ContentHeader from 'components/content-header'
+import Docs from "components/docs"
+
+import {
+  docsPanelProfile,
+  docsSupportAssets,
+} from "pages/docs-props"
 
 import  './media.scss'
 
@@ -19,94 +25,228 @@ export default class Media extends Component {
         </div>
         <div className="mediaConatiner md-grid">
           <div className="mediaCell md-cell md-cell--4">
-            <PanelProfile
-              urlImg="http://i.pravatar.cc/150?img=11"
-              profileName="Instructor Name"
-              adress="Tunis, Tunisia"
-              tel="00218867234"
-              Email="jobran@gmail.com"
-              className="media"
-              colorHeader="bleuGardian"
-            />
-            <PanelProfile
-              urlImg="http://i.pravatar.cc/150?img=11"
-              profileName="Instructor Name"
-              adress="Tunis, Tunisia"
-              tel="00218867234"
-              Email="jobran@gmail.com"
-              className="media"
-              colorHeader="light"
-            />
+            <Docs
+              title={"PanelProfile"}
+              docsProps={docsPanelProfile}
+              dataCode={`
+              <PanelProfile
+                urlImg="http://i.pravatar.cc/150?img=11"
+                profileName="Instructor Name"
+                adress="Tunis, Tunisia"
+                tel="00218867234"
+                Email="jobran@gmail.com"
+                className="media"
+                colorHeader="bleuGardian"
+              />
+              `}
+            >
+              <PanelProfile
+                urlImg="http://i.pravatar.cc/150?img=11"
+                profileName="Instructor Name"
+                adress="Tunis, Tunisia"
+                tel="00218867234"
+                Email="jobran@gmail.com"
+                className="media"
+                colorHeader="bleuGardian"
+              />
+            </Docs>
+            <Docs
+              title={"PanelProfile"}
+              docsProps={docsPanelProfile}
+              dataCode={`
+              <PanelProfile
+                urlImg="http://i.pravatar.cc/150?img=11"
+                profileName="Instructor Name"
+                adress="Tunis, Tunisia"
+                tel="00218867234"
+                Email="jobran@gmail.com"
+                className="media"
+                colorHeader="bleuGardian"
+              />
+              `}
+            >
+              <PanelProfile
+                urlImg="http://i.pravatar.cc/150?img=11"
+                profileName="Instructor Name"
+                adress="Tunis, Tunisia"
+                tel="00218867234"
+                Email="jobran@gmail.com"
+                className="media"
+                colorHeader="light"
+              />
+            </Docs>
             <div className="md-grid">
               <div className="mediaCell md-cell md-cell--6">
-                <PanelProfile
-                  urlImg="http://i.pravatar.cc/150?img=11"
-                  profileName="Instructor Name"
-                  className="media"
-                  responsive
-                  colorHeader="bleuGardian"
-                />
+                <Docs
+                  title={"PanelProfile"}
+                  docsProps={docsPanelProfile}
+                  dataCode={`
+                  <PanelProfile
+                    urlImg="http://i.pravatar.cc/150?img=11"
+                    profileName="Instructor Name"
+                    adress="Tunis, Tunisia"
+                    tel="00218867234"
+                    Email="jobran@gmail.com"
+                    className="media"
+                    colorHeader="bleuGardian"
+                  />
+                  `}
+                >
+                  <PanelProfile
+                    urlImg="http://i.pravatar.cc/150?img=11"
+                    profileName="Instructor Name"
+                    className="media"
+                    responsive
+                    colorHeader="bleuGardian"
+                  />
+                </Docs>
               </div>
               <div className="mediaCell md-cell md-cell--6">
-                <PanelProfile
-                  urlImg="http://i.pravatar.cc/150?img=11"
-                  profileName="Instructor Name"
-                  className="media"
-                  responsive
-                  colorHeader="light"
-                />
+                <Docs
+                  title={"PanelProfile"}
+                  docsProps={docsPanelProfile}
+                  dataCode={`
+                  <PanelProfile
+                    urlImg="http://i.pravatar.cc/150?img=11"
+                    profileName="Instructor Name"
+                    adress="Tunis, Tunisia"
+                    tel="00218867234"
+                    Email="jobran@gmail.com"
+                    className="media"
+                    colorHeader="bleuGardian"
+                  />
+                  `}
+                >
+                  <PanelProfile
+                    urlImg="http://i.pravatar.cc/150?img=11"
+                    profileName="Instructor Name"
+                    className="media"
+                    responsive
+                    colorHeader="light"
+                  />
+                </Docs>
               </div>
             </div>
           </div>
           <div className="mediaCell md-cell md-cell--3">
             <div className="media">
-              <SupportAssets
-                type="img"
-                title="Project images"
-                description="Project details and information"
-                url="https://picsum.photos/300/200?image=0"
-                actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
-              />
+              <Docs
+                title={"SupportAssets"}
+                docsProps={docsSupportAssets}
+                dataCode={`
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+                `}
+              >
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+              </Docs>
             </div>
             <div className="media">
-              <SupportAssets
-                type="img"
-                responsive
-                title="Project images"
-                description="Project details and information"
-                url="https://picsum.photos/300/200?image=0"
-                actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
-              />
+              <Docs
+                title={"SupportAssets"}
+                docsProps={docsSupportAssets}
+                dataCode={`
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+                `}
+              >
+                <SupportAssets
+                  type="img"
+                  responsive
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+              </Docs>
             </div>
             <div className="media">
-              <SupportAssets
-                type="img"
-                responsive
-                title="Project images"
-                description="Project details and information"
-                url="https://picsum.photos/300/200?image=0"
-                className="costumColor"
-              />
+              <Docs
+                title={"SupportAssets"}
+                docsProps={docsSupportAssets}
+                dataCode={`
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+                `}
+              >
+                <SupportAssets
+                  type="img"
+                  responsive
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  className="costumColor"
+                />
+              </Docs>
             </div>
           </div>
           <div className="mediaCell md-cell md-cell--5">
             <div className="media">
-              <SupportAssets
-                type="video"
-                title="Project images"
-                playlist
-                poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
-                url="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
-              />
+              <Docs
+                title={"SupportAssets"}
+                docsProps={docsSupportAssets}
+                dataCode={`
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+                `}
+              >
+                <SupportAssets
+                  type="video"
+                  title="Project images"
+                  playlist
+                  poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
+                  url="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+                />
+              </Docs>
             </div>
             <div className="media">
-              <SupportAssets
-                type="img"
-                title="Project images"
-                description="Project details and information"
-                url="https://picsum.photos/500/200?image=0"
-                actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
-              />
+              <Docs
+                title={"SupportAssets"}
+                docsProps={docsSupportAssets}
+                dataCode={`
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/300/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+                `}
+              >
+                <SupportAssets
+                  type="img"
+                  title="Project images"
+                  description="Project details and information"
+                  url="https://picsum.photos/500/200?image=0"
+                  actionButton={()=> <Button icon onClick={this.plus} iconClassName="mdi mdi-plus" />}
+                />
+              </Docs>
             </div>
           </div>
         </div>
